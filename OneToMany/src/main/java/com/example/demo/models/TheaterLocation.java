9e27094899d69c7fdb3a67ea4be9699c83b29cb6
Tara Johnson <tara@madface.com>
2018-08-04 16:15:34 -0700

    Add movie company working.

--- OneToMany/src/main/java/com/example/demo/models/TheaterLocation.java
+++ OneToMany/src/main/java/com/example/demo/models/TheaterLocation.java
@@ -7,9 +7,13 @@ import javax.persistence.*;
 public class TheaterLocation {
     @Id
     public Long id;
+    @Column(name = "moviecompanyid")
     public Long movieCompanyId;
     public String name;
     public String location;
+
+    // required default constructor
+    public TheaterLocation() {}
 
     public TheaterLocation(long movieCompanyId, String name, String location) {
         this.movieCompanyId = movieCompanyId;
